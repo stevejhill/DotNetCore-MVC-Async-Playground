1c6808071c57517807810835c676a1285ed645fa
Steve Hill <steve@Steves-MacBook-Pro.local>
2016-11-29 13:09:26 -0500

    Upgraded tp .net core 1.1 Minor changes to home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,63 +15,44 @@ namespace MVC_Async_Playground.Controllers
 			[HttpPost]
 			public async Task<IActionResult> RunSync()
 			{
-          var stopWatch = new Stopwatch();
-					stopWatch.Start();
- 					await LongRunningSync();
-					//return RedirectToAction("Done", "Home");
-					stopWatch.Stop();
-					ViewData["ElapsedTime"] = stopWatch.Elapsed.TotalMilliseconds;
-					return View();
+          		var stopWatch = new Stopwatch();
+				stopWatch.Start();
+ 				await RunDelay2SecondsMethod1();
+				await RunDelay2SecondsMethod2();
+				await RunDelay2SecondsMethod1();
+				await RunDelay2SecondsMethod2(); 
+				stopWatch.Stop();
+				ViewData["ElapsedTime"] = stopWatch.Elapsed.TotalMilliseconds;
+				return View();
 			}
 
 			[HttpPost]
 			public async Task<IActionResult> RunASync()
 			{
-          var stopWatch = new Stopwatch();
-					stopWatch.Start();
- 					await LongRunningASync();
-					//return RedirectToAction("Done", "Home");
-					stopWatch.Stop();
-					ViewData["ElapsedTime"] = stopWatch.Elapsed.TotalMilliseconds;
-					return View();
+          		var stopWatch = new Stopwatch();
+				stopWatch.Start();
+ 				await Task.WhenAll(RunDelay2SecondsMethod1(),RunDelay2SecondsMethod2(),RunDelay2SecondsMethod1(),RunDelay2SecondsMethod2());
+				stopWatch.Stop();
+				ViewData["ElapsedTime"] = stopWatch.Elapsed.TotalMilliseconds;
+				return View();
 			}
 
 			public IActionResult Done()
 			{
 				return View();
 			}
+			
 
-
-			private async Task LongRunningSync()
+			private Task RunDelay2SecondsMethod1()
 			{
-				await Task.Run(() => {
-					Thread.Sleep(2000);
-				});
-
-				await Task.Delay(2000);
-				await Task.Delay(2000);
-				await Task.Delay(2000);
-				await Task.Delay(2000);
-				await Task.Delay(2000);
-
-				return;
+				return Task.Delay(2000);
 			}
 
-			private async Task LongRunningASync()
+			private Task RunDelay2SecondsMethod2()
 			{
-				var task1 = Task.Run(() => {
+				return Task.Run(() => {
 					Thread.Sleep(2000);
-				});
-
-				var task2 = Task.Delay(2000);
-				var task3 = Task.Delay(2000);
-				var task4 = Task.Delay(2000);
-				var task5 = Task.Delay(2000);
-				var task6 = Task.Delay(2000);
-
-				await Task.WhenAll(task1, task2, task3, task4, task5);
-
-				return;
+					});
 			}
 	}
 }
